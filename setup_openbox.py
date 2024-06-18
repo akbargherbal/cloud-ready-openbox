8b987ee28520049662a39122a3c7638cd6df97ae
akbargherbal <51210927+akbargherbal@users.noreply.github.com>
2024-06-18 12:46:57 +0300

    Add files via upload

--- setup_openbox.py
+++ setup_openbox.py
@@ -9,6 +9,9 @@ from datetime import datetime
 TIME_STAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
 log_file_name = os.path.join(os.getenv("HOME"), f'setup_openbox_{TIME_STAMP}.log')
 
+# Add a print statement to verify log file path
+print(f"Log file path: {log_file_name}")
+
 logging.basicConfig(filename=log_file_name, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s', filemode='w')
 
